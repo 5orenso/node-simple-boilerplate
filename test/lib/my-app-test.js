92a364ab4bdfd52fae6a39b537af3a809219a32c
Øistein Sørensen <sorenso@gmail.com>
2015-03-17 17:41:12 +0100

    Fixed due to new module layout, and fixed code style.

---
+++ test/lib/my-app-test.js
@@ -0,0 +1,63 @@
+/*
+ * https://github.com/5orenso
+ *
+ * Copyright (c) 2014 Øistein Sørensen
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+var buster = require('buster'),
+    assert = buster.assert,
+    refute = buster.refute,
+    when   = require('when'),
+    appPath = __dirname + '/../../';
+
+buster.testCase('lib/logger', {
+    setUp: function () {
+    },
+    tearDown: function () {
+        delete require.cache[require.resolve(appPath + 'lib/my-app')];
+    },
+    'Test module:': {
+        'dummy sync test': function () {
+            assert(true);
+        },
+
+        'dummy async test': function (done) {
+            // Do some async stuff and call done.
+            assert(true);
+            done();
+        },
+
+        'dummy async w/promises test': function (done) {
+            when(function functionWhichReturnsAPromise() {
+                return 'my promise';
+            })
+                .done(function success(result) {
+                    assert(true);
+                    done();
+                }, function error(result) {
+                    assert(true);
+                    done();
+                });
+        },
+
+        'exposed run function w/mock services': function () {
+            var logger = {
+                log: this.spy()
+            };
+            var myApp = require(appPath + 'lib/my-app')({}, {logger: logger});
+            myApp.run();
+            assert.called(logger.log);
+        },
+
+        'exposed run function w/options': function () {
+            var logger = {
+                log: this.spy()
+            };
+            var myApp = require(appPath + 'lib/my-app')({logger: logger});
+            myApp.run();
+            assert.called(logger.log);
+        }
+    }
+});
