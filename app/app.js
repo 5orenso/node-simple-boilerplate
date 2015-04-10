8e494de63fe345cb3f208a66238e73c87f2b44db
Øistein Sørensen <sorenso@gmail.com>
2015-04-10 15:02:41 +0200

    Added config validation.

--- app/app.js
+++ app/app.js
@@ -8,13 +8,19 @@
 
 var path = require('path'),
     commander = require('commander'),
-    appPath = path.normalize(__dirname + '/../');
+    appPath = path.normalize(__dirname + '/../'),
+    ConfigValidator = require(appPath + 'lib/config-validator'),
+    configValidator = new ConfigValidator();
 
 commander
     .option('-c, --config <file>', 'configuration file path', './config/config-dist.js')
     .option('-s, --server <server ip>', 'ip of server', '127.0.0.1')
     .parse(process.argv);
 var config = require(appPath + commander.config);
+if (!configValidator.isValid(config)) {
+    console.error(configValidator.error());
+    process.exit(1);
+}
 
 var Logger = require(appPath + 'lib/logger');
 var logger = new Logger({
