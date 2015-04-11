7da7120d5d9d063c15928e66d5b5cb311b428e70
Øistein Sørensen <sorenso@gmail.com>
2015-04-11 15:47:31 +0200

    Using config loader.

--- app/app.js
+++ app/app.js
@@ -8,28 +8,27 @@
 
 var path = require('path'),
     commander = require('commander'),
-    appPath = path.normalize(__dirname + '/../'),
-    ConfigValidator = require(appPath + 'lib/config-validator'),
-    configValidator = new ConfigValidator();
+    appPath = path.normalize(__dirname + '/../');
 
 commander
     .option('-c, --config <file>', 'configuration file path', './config/config-dist.js')
     .option('-s, --server <server ip>', 'ip of server', '127.0.0.1')
     .parse(process.argv);
-var config = require(appPath + commander.config);
-if (!configValidator.isValid(config)) {
-    console.error(configValidator.error());
-    process.exit(1);
+
+var ConfigLoader = require(appPath + 'lib/config-loader');
+var configLoader = new ConfigLoader();
+var config = configLoader.load(appPath + commander.config);
+
+if (config) {
+    var Logger = require(appPath + 'lib/logger');
+    var logger = new Logger({
+        logLevel: config.logLevel
+    });
+
+    var App = require(appPath + 'lib/my-app.js');
+    var app = new App({
+        logger: logger
+    });
+
+    app.run();
 }
-
-var Logger = require(appPath + 'lib/logger');
-var logger = new Logger({
-    logLevel: config.logLevel
-});
-
-var App = require(appPath + 'lib/my-app.js');
-var app = new App({
-    logger: logger
-});
-
-app.run();
