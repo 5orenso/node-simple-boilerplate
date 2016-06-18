2a846df06ad2581d090f234cbd05df88902bf6b0
Øistein Sørensen <sorenso@gmail.com>
2016-06-18 21:09:34 +0200

    Fixed some small bugs.

--- install.sh
+++ install.sh
@@ -3,16 +3,14 @@
 echo 'You should probably do this first:'
 echo '1. Generate a new repo on github.com.'
 echo '2. Clone repo to you project home dir.'
-
 echo '3. Enter path of new repo without trailing slash: '
 read PATH
 
 # Make paths we need
-/bin/mkdir $PATH/logs
+/bin/mkdir -p $PATH/logs
 
 # Copying paths
 /bin/cp -Rv app $PATH/.
-/bin/cp -Rv config $PATH/.
 /bin/cp -Rv lib $PATH/.
 /bin/cp -Rv test $PATH/.
 
@@ -31,13 +29,14 @@ read PATH
 /bin/cp -v run-app.sh $PATH/.
 /bin/cp -v run-tests.sh $PATH/.
 /bin/cp -v run-watch.sh $PATH/.
+/bin/cp -v npm-release.sh $PATH/.
 
 cd $PATH
 /bin/cp -v ./.coveralls-dist.yml ./.coveralls.yml
 
 echo ''
 echo 'Todo:'
-echo '1. Update coverlalls.yml with the correct token.'
+echo '1. Update coveralls.yml with the correct token.'
 echo '2. Run `npm install`'
 echo '3. Run you app `./run-app.sh`.'
 echo ''
