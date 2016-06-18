0f989231922ae23250be0fef9a6c9659236a03ea
Øistein Sørensen <sorenso@gmail.com>
2016-06-18 19:50:45 +0200

    Simplified. Removed logger and config.

--- app/app.js
+++ app/app.js
@@ -1,34 +1,15 @@
 /*
  * https://github.com/5orenso
  *
- * Copyright (c) 2014 Øistein Sørensen
+ * Copyright (c) 2016 Øistein Sørensen
  * Licensed under the MIT license.
  */
 'use strict';
 
 var path = require('path'),
-    commander = require('commander'),
     appPath = path.normalize(__dirname + '/../');
 
-commander
-    .option('-c, --config <file>', 'configuration file path', './config/config-dist.js')
-    .option('-s, --server <server ip>', 'ip of server', '127.0.0.1')
-    .parse(process.argv);
+var App = require(appPath + 'lib/my-app');
+var app = new App();
 
-var ConfigLoader = require(appPath + 'lib/config-loader');
-var configLoader = new ConfigLoader();
-var config = configLoader.load(appPath + commander.config);
-
-if (config) {
-    var Logger = require(appPath + 'lib/logger');
-    var logger = new Logger({
-        logLevel: config.logLevel
-    });
-
-    var App = require(appPath + 'lib/my-app.js');
-    var app = new App({
-        logger: logger
-    });
-
-    app.run();
-}
+module.exports = app;
